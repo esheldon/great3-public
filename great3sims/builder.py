9d115920404847d71f4206505276d28ac5f7285b
Erin Sheldon <erin.sheldon@gmail.com>
2015-02-26 16:47:59 -0500

    parallelization at file level as suggested by rachel

--- great3sims/builder.py
+++ great3sims/builder.py
@@ -770,12 +770,13 @@ class SimBuilder(object):
         }
         d['gal']['magnification'] = { 'type' : 'Catalog', 'col' : 'mu' }
 
-        if not self.variable_psf:
-            # The galaxy images are large, so parallelize at the image level (unlike for the small
-            # star fields for constant PSF, for which we already had set up to parallelize at the
-            # file level).
-            d['image']['nproc'] = self.nproc
-            del d['output']['nproc']
+        # ESS: parallization at the file level
+        #if not self.variable_psf:
+        #    # The galaxy images are large, so parallelize at the image level (unlike for the small
+        #    # star fields for constant PSF, for which we already had set up to parallelize at the
+        #    # file level).
+        #    d['image']['nproc'] = self.nproc
+        #    del d['output']['nproc']
 
         if self.real_galaxy:
             # Need to add the RealGalaxyCatalog to input.
